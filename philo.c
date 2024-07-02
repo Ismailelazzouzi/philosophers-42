9c3bf1a7e44091f8a53188483625fe0bd8b45691
ismail el azzouzi <ismailazzouzi.contact@gmail.com>
2024-07-02 07:11:07 +0100

    parse

--- philo.c
+++ philo.c
@@ -6,8 +6,10 @@ int main(int argc, char **argv)
 	if (argc == 5 || argc == 6)
 	{
 		parse(&table, argv);
-		printf("%lu\n%lu\n%lu\n%lu\n%lu\n", table.philo_nbr, 
-		table.t_t_d, table.t_t_e, table.t_t_s, table.meals_limit);
+		printf("%lu\n%lu\n%lu\n%lu\n", table.philo_nbr, 
+		table.t_t_d, table.t_t_e, table.t_t_s);
+		if (argc == 6)
+			printf("%lu\n", table.meals_limit);
 	}
 	else
 		printf("invalid input\n");
